c983f7bf53cac2f86adf3ae4a3139e827a1669d5
willstocks-tech <43744514+willstocks-tech@users.noreply.github.com>
2019-02-12 11:09:02 +0000

    Rework dynamicPolyfill to polyfill all at once
    `dynamicPolyfill()` was passing each polyfill item through to polyfill.io individually, rather than all at once. This was resulting in each item within an array (for example, 3 items needing polyfill) having it's own polyfill.io request - a waste of 2 requests for example!
    
    Furthermore, if nothing needed polyfill, we were still calling out to polyfill.io for absolutely nothing... a wasted request!

--- dynamicpolyfill.js
+++ dynamicpolyfill.js
@@ -1,103 +1,125 @@
 function dynamicPolyfill(tocheck, scriptToPolyfill, functionToRunonLoad) {
-	var checkPromises = [];
-	if(Array.isArray(tocheck)) {
-		tocheck.forEach(
-			function(tocheck) {
-				checkPromises.push(checking(tocheck))
-			}
-		)
-	} else {
-		checkPromises.push(checking(tocheck))
-	}
-	Promise.all(checkPromises).then(
-		function() {
-			loadMyScript(scriptToPolyfill, functionToRunonLoad)
-		}
-	).catch(function(error){return error})
+    var polyfillReq = [];
+    if(Array.isArray(tocheck)) {
+        tocheck.forEach(
+            function(tocheck) {
+                polyfillReq.push(checking(tocheck))
+            }
+        )
+    } else {
+        polyfillReq.push(checking(tocheck))
+    }
+    polyfillReq =
+        polyfillReq
+        .filter(
+            function(p) {
+                return p !== undefined
+            }
+        )
+        .join(' ');
+    loadPolyfill(polyfillReq, scriptToPolyfill, functionToRunonLoad);
 }
 
 function checking(check) {
-	var splitChars = '.';
-	var split = check.split(splitChars);
-	var firstWord = window[split[0]];
-	var lastWord = new Object(split[split.length - 1]);
-	if(typeof (window.check) == 'undefined' || !check in window || (check.indexOf(splitChars) >= 1 && !lastWord in firstWord) || !check in this) {
-		loadPolyfill(check);
-	}
+    var splitChars = '.';
+    var split = check.split(splitChars);
+    var firstWord = window[split[0]];
+    var lastWord = new Object(split[split.length - 1]);
+    if (check in window == false || check in this == false) {
+        if (check.indexOf(splitChars) >= 1) {
+            if (lastWord in firstWord == false) {
+                return check
+            }
+        } else {
+            return check
+        }
+    }
 }
 
-function loadPolyfill(url) {
-	return new Promise(
-		function(resolve, reject) {
-			var polyfill = document.createElement('script');
-			polyfill.src = ('https://polyfill.io/v3/polyfill.min.js?features=' + encodeURIComponent(url));
-			document.body.appendChild(polyfill);
-			polyfill.onerror = function(response) {
-				return reject("Loading the polyfill(s) failed!", response)
-			}
-			polyfill.onload = function() {
-				return resolve
-			}
+function loadPolyfill(url, scriptToPolyfill, functionToRunonLoad) {
+	if(url !== "") {
+		var polyfill = document.createElement('script');
+		polyfill.src = ('https://polyfill.io/v3/polyfill.min.js?features='+encodeURIComponent(url));
+		polyfill.onerror = function(response) {
+			console.error("Loading the polyfill(s) failed!", response);
+		} 
+		polyfill.onload = function() {
+			loadMyScript(scriptToPolyfill, functionToRunonLoad)
 		}
-	)
+        document.body.appendChild(polyfill);
+	} else {
+	    loadMyScript(scriptToPolyfill, functionToRunonLoad)
+	}
 }
 
 function loadMyScript(url, functionToRunonLoad) {
-	if(Array.isArray(url)) {
-		var promises = [];
-		url.forEach(
-			function(url) {
-			    promises.push(nonblankURL(url))
-		    }
-		);
-		Promise.all(promises).then(
-    			function() {
-				initialiseMyScript(functionToRunonLoad)
-    			}
-		).catch(function(error){return error})
-	} else if (!Array.isArray(url) && url !== null && url !== '') {
-		nonblankURL(url).then(
-    			function() {
-				initialiseMyScript(functionToRunonLoad)
-   			}
-		).catch(function(error){return error})
-	} else {
-		initialiseMyScript(functionToRunonLoad)
-	}
+    if (Array.isArray(url)) {
+        var promises = [];
+        url.forEach(
+            function(url) {
+                promises.push(nonblankURL(url))
+        );
+        Promise.all(promises)
+            .then(
+                function() {
+                    initialiseMyScript(functionToRunonLoad)
+                }
+            )
+            .catch(
+                function(error) {
+                    return error
+                }
+            )
+    } else if (!Array.isArray(url) && url !== null && url !== '') {
+        nonblankURL(url)
+            .then(
+                function() {
+                    initialiseMyScript(functionToRunonLoad)
+                }
+            )
+            .catch(
+                function(error) {
+                    return error
+                }
+            )
+    } else {
+        initialiseMyScript(functionToRunonLoad)
+    }
 }
 
 function nonblankURL(uri) {
-	return new Promise(
-		function(resolve, reject) {
-			var thescript = document.createElement('script');
-			thescript.src = encodeURI(uri);
-			document.body.appendChild(thescript);
-			thescript.onerror = function(response) {
-				return reject("Loading the script failed!", response)
-			}
-			thescript.onload = function() {
-				return resolve(uri)
-			}
-		}
-	)
+    return new Promise (
+        function(resolve,reject) {
+            var thescript = document.createElement('script');
+            thescript.src = encodeURI(uri);
+            thescript.onerror = function(response) {
+                return reject('Loading the script failed!', response)
+            }
+            thescript.onload = function() {
+                return resolve(uri)
+            }
+            document.body.appendChild(thescript)
+        }
+    )
 }
 
 function initialiseMyScript(functionToRunonLoad) {
-	if(Array.isArray(functionToRunonLoad)) {
-		functionToRunonLoad.forEach(
-			function(functionToRunonLoad) {
-				initScript(functionToRunonLoad)
-			}
-		)
-	} else {
-		initScript(functionToRunonLoad)
-	}
-	function initScript(fn) {
-		try {
-			window[fn]
-		}
-		catch(err) {
-			console.error('There was an error: ', err, err.name, err.stack)
-		}
-	}
+    if (Array.isArray(functionToRunonLoad)) {
+        functionToRunonLoad.forEach(
+            function(functionToRunonLoad) {
+                initScript(functionToRunonLoad)
+            }
+        )
+    } else {
+        initScript(functionToRunonLoad)
+    }
+    function initScript(fn) {
+        try {
+            var run = new Function(fn);
+	        run()
+        }
+        catch (err) {
+            console.error('There was an error: ', err, err.name, err.stack)
+        }
+    }
 }
